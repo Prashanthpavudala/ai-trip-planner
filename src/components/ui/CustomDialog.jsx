bbee08d4b6ee97d4f12b64cc46fd84f994fc3676
Navyakumar98 <navyakumar.2259@gmail.com>
2025-02-10 12:23:42 +0530

    refactoring

--- src/components/ui/CustomDialog.jsx
+++ src/components/ui/CustomDialog.jsx
@@ -10,18 +10,15 @@ export const CustomDialog = ({ isOpen, onClose, login }) => {
     <div className={`${isOpen ? "fixed inset-0 flex items-center justify-center bg-black/50" : "hidden"}`}>
       <div className="bg-white rounded-lg shadow-lg p-6 w-96">
         <div className="flex flex-col items-center">
-          {/* Logo */}
+
           <img src="/logo.svg" alt="Logo" className="w-16 h-16 mb-4" />
 
-          {/* Title */}
           <h2 className="font-bold text-lg mt-2">Sign In With Google</h2>
 
-          {/* Description */}
           <p className="text-gray-600 text-center mt-2">
             Sign in to the App with Google authentication securely.
           </p>
 
-          {/* Button */}
           <button
             className="mt-4 px-4 py-2 bg-black text-white rounded-md hover:bg-gray-800 flex items-center gap-2"
             onClick={() => {
