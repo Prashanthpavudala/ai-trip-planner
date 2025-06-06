c3cd687b02dc8c3f81692968215fc7d3c596b96f
Navyakumar98 <navyakumar.2259@gmail.com>
2025-06-07 00:39:39 +0530

    Text fix

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -6,9 +6,9 @@ function Hero() {
   return (
     <div className='flex flex-col items-center mx-56 gap-9'>
         <h1 className='font-extrabold text-[35px] text-center mt-16'>
-           <span className='text-[#f56551]'>Discover Next Adventure with AI:</span> <br></br> Personalised Itineraries at Your FingerTips 
+           <span className='text-[#f56551]'>Discover Next Adventure with TripGenie</span> <br></br> Personalised Itineraries at Your FingerTips 
         </h1>
-        <p className='text-xl text-gray-500 text-center'> Your personal trip planner and travel curator, creating custom itineraries tailored to your intrests and budget. </p>   
+        <p className='text-xl text-gray-500 text-center'> Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget. </p>   
         <Link to={'/create-trip'}>
              <Button> Get Started, Its Free</Button>
         </Link>
